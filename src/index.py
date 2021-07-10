d15899b8c23afd0624400395c44b34fb6bbf4383
Joubert Saquett <43321295+joubertsaquett@users.noreply.github.com>
2021-07-10 16:14:48 -0300

    ajuste de execução

--- src/index.py
+++ src/index.py
@@ -3,6 +3,7 @@ from routes.userRoute import user
 
 app = Flask(__name__)
 app.register_blueprint(user)
-
-if __name__ == '__main__': 
+    
+if __name__ == '__main__':
+    print("initiating the web app...")
     app.run(debug=True)
